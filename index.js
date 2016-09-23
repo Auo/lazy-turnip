e79cdefc6a67e65d370c29a4a775a2d845661d71
Auo <andreas.lindgren@outlook.com>
2016-09-23 13:08:05 +0200

    cleanup of index.js + preparation for autoupdater

--- index.js
+++ index.js
@@ -2,81 +2,12 @@
 const electron = require('electron');
 const path = require('path')
 const os = require('os')
+const updater = require('./lib/appUpdater.js')
 const app = electron.app;
 
 
 
-
-
-//FOR INSTALLER AND AUTO-UPDATER! (ska detta vara överst?)
-
-if (handleSquirrelEvent()) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-
-function handleSquirrelEvent() {
-  if (process.argv.length === 1) {
-    return false;
-  }
-
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function(command, args) {
-    let spawnedProcess, error;
-
-    try {
-      spawnedProcess = ChildProcess.spawn(command, args, {detached: true});
-    } catch (error) {}
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function(args) {
-    return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-    case '--squirrel-install':
-    case '--squirrel-updated':
-      // Optionally do things such as:
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Install desktop and start menu shortcuts
-      spawnUpdate(['--createShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Remove desktop and start menu shortcuts
-      spawnUpdate(['--removeShortcut', exeName]);
-
-      setTimeout(app.quit, 1000);
-      return true;
-
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-
-      app.quit();
-      return true;
-  }
-};
-
+if(require('electron-squirrel-startup')) return;
 
 
 
@@ -84,6 +15,7 @@ function handleSquirrelEvent() {
 require('electron-debug')();
 
 require('electron-reload')(__dirname);
+
 
 // prevent window being garbage collected
 let mainWindow;
@@ -139,4 +71,9 @@ if (shouldQuit) {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
+
+  //We should check if there is a new version available.
+	//For now, let's not check
+	//updater();
+
 });
