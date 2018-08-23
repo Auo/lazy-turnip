1d6f778720e424bef28d5457af2f995da1f64959
Andreas Lindgren <andreas.lindgren@outlook.com>
2018-08-23 09:04:59 +0200

    additional linting fixes

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const os = require('os');
 const updater = require('./lib/appUpdater.js');
 const app = electron.app;
 
-if(require('electron-squirrel-startup')) return;
+if (require('electron-squirrel-startup')) return;
 
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 require('electron-reload')(__dirname);
 
-
 // prevent window being garbage collected
-let mainWindow;
+// eslint-disable-next-line
+let mainWindow = null;
 
 function onClosed() {
 	// dereference the window
@@ -25,9 +25,9 @@ function createMainWindow() {
 	const win = new electron.BrowserWindow({
 		width: 857,
 		height: 720,
-		icon: path.join(__dirname,'images','logo.ico'),
-    minWidth:200,
-    minHeight:150,
+		icon: path.join(__dirname, 'images', 'logo.ico'),
+    minWidth: 200,
+    minHeight: 150,
     titleBarStyle: 'hidden',
     frame: os.platform() !== 'win32'
 	});
@@ -39,36 +39,22 @@ function createMainWindow() {
 }
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+	if (process.platform !== 'darwin') app.quit();
 });
 
-// app.on('activate', () => {
-// 	if (!mainWindow) {
-// 		mainWindow = createMainWindow();
-// 	}
-// });
-//
-
-
-const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+const shouldQuit = app.makeSingleInstance(() => {
   // Someone tried to run a second instance, we should focus our window.
-  if (myWindow) {
-    if (myWindow.isMinimized()) myWindow.restore();
-    myWindow.focus();
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) mainWindow.restore();
+    mainWindow.focus();
   }
 });
 
-if (shouldQuit) {
-  app.quit();
-}
+if (shouldQuit) app.quit();
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
-
   //We should check if there is a new version available.
 	//For now, let's not check since the server isn't running anymore (uncomment this if you want auto-update)
 	updater();
-
 });
