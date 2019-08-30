0079d5969806f5008d9d41bdd8711990b871e6a9
Andreas Lindgren <andreas.lindgren@outlook.com>
2019-08-30 16:27:06 +0200

    Fix missing files

---
+++ viewModels/settingsViewModel.js
@@ -0,0 +1,41 @@
+const ko = require('knockout');
+const { dialog } = require('electron').remote;
+const { shell } = require('electron');
+
+class SettingsViewModel {
+	constructor(app) {
+		this.app = app;
+		this.isConfigMissing = ko.observable(false);
+		this.folderChangeCallback = null;
+		this.version = ko.observable(require('electron').remote.app.getVersion());
+	}
+
+	selectFolder() {
+		const directories = dialog.showOpenDialog({ properties: ['openDirectory'] });
+
+		if (!!directories)
+			this.app.storage.getConfig(config => {
+				config.addonfolder = directories[0];
+				this.app.storage.setConfig(config, () => {
+					this.isConfigMissing(false);
+					if (this.folderChangeCallback != null && typeof this.folderChangeCallback == 'function')
+						this.folderChangeCallback();
+				});
+			});
+	}
+
+	openFolder() {
+		this.app.storage.getConfig(config => shell.openItem(config.addonfolder));
+	}
+
+	visitProject() {
+		shell.openExternal('https://github.com/auo/lazy-turnip');
+	}
+
+	init(cb) {
+		this.app.storage.getConfig(config => this.isConfigMissing(this.app.storage.missingConfig(config)));
+		this.folderChangeCallback = cb;
+	}
+}
+
+module.exports = (app) => new SettingsViewModel(app);
